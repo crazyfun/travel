54a3fca44037f11488818a1ffec61ae16ffb57ec
root <root@west136135.(none)>
2013-07-01 01:06:25 +0800

    init

---
+++ gitrun.sh
@@ -0,0 +1,8 @@
+git remote travel
+git init
+git remote add travel gitadmin@211.149.149.135:/home/gitadmin/gitshare/travel.git
+git pull travel master
+git add .
+git commit -m "init"
+git push travel master
+
